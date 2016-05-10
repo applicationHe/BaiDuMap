8a99bf7edd1ebc447ebc03eed51e4bd8100aa026
何万牡 <931280409@qq.com>
2016-05-10 13:47:20 +0800

    Initial Commit

---
+++ baiduMapDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  baiduMapDemo
+//
+//  Created by apple on 16/5/10.
+//  Copyright © 2016年 何万牡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ baiduMapDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  baiduMapDemo
+//
+//  Created by apple on 16/5/10.
+//  Copyright © 2016年 何万牡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
